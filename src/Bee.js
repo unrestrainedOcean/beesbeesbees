cefa7f4b8548f46969284e049e628de3574af123
Rafael <makoralphie@yahoo.com>
2022-04-18 11:50:04 -0700

    All propmts passed in BEEEEEEES exercise

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  // Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = new Grub; // give us our Grub obj
+
+// our protype contructor is looking at grub
+Bee.prototype.constructor = Bee;
+// NOW our protype contructor is looking at Bee
+
+
 
